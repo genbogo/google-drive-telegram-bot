fc911b72fc6d408b6cc9d03ecf4ddc6a4f80c6ea
Genbogo <66338822+genbogo@users.noreply.github.com>
2021-04-27 04:49:04 +0000

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "1714327530:AAEN3PFOXIg6jzUcSiUZaE_Wq4zqEApfdXc"
     APP_ID = "1526915"
     API_HASH = "f4c43c3a44c806e314416d5dcdf10443"
-    DATABASE_URL = "postgres://cqbamoagebgyve:291561a8e3f1438c1cff044f8132ef9ecc621f38dba866d9179ac7ff6118d354@ec2-54-224-120-186.compute-1.amazonaws.com:5432/d6vs3b6v9o4oq7"
+    DATABASE_URL = "postgres://cqbamoagebgyve.amazonaws.com:5432/d6vs3b6v9o4oq7postgres://nsuaorfwgxgrja:3d68ac382de84d1fefa982bc9a5dab28f261b82570ef3364e7798a92c46fb88a@ec2-107-20-153-39.compute-1.amazonaws.com:5432/d9s967olomi225"
     SUDO_USERS = "1087446551" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
